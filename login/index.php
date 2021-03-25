d1e45f95539dd5fd67ede4e1f269252c554f5209
abifathullah <abifathullah@users.noreply.github.com>
2021-03-26 00:36:45 +0800

    commit

--- login/index.php
+++ login/index.php
@@ -1,3 +1,4 @@
 <form method="post">
-    Username: <input type="text" name="username" id="username">
+    Username: <input type="text" name="username" id="username"><br/>
+    Password: <input type="password" name="password" id="password">
 </form>

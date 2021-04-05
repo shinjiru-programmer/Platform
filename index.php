f40dfd96570696e4925eaaffff4eee2df9ec6235
Abi Fathullah Abdullah <abifathullah@icloud.com>
2021-04-05 21:08:06 +0800

    resource format

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
     </head>
     <body>
 

68e88aa2f6e46bcb3a97267036b7a465c88de68b
Abi Fathullah Abdullah <abifathullah@icloud.com>
2021-04-06 06:29:39 +0800

    commit

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
-<?php session_start(); ?>
-
 <!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
     </head>
     <body>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
 
         <div class="container-fluid bg-light pt-2 pb-2 border">
             <div class="col-md-12 text-center"><h4>Platform</h4></div>
         </div>
 
         <div class="container mt-4">
-
             <form action="sys_login.php" method="post">
                 <div class="row justify-content-md-center">
                     <div class="col-md-2 mb-3 text-sm-start text-md-end">Username</div>
@@ -36,7 +33,6 @@
                     </div>
                 </div>
             </form>
-
         </div>
         
     </body>
